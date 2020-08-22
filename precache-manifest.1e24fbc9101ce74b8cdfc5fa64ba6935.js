c59d90e9376b801d3b2a86884d2a85399fd8d2ca
birriaestilojalisco <lazer3p.inc@gmail.com>
2020-08-22 11:38:58 -0700

    Updates

---
+++ precache-manifest.1e24fbc9101ce74b8cdfc5fa64ba6935.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cc557e5c75c00a3f476b80c8282a89",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "28e6be03345479154e22",
+    "url": "/home/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "9ae706dfd53fc5e2c7a4",
+    "url": "/home/static/js/2.19709df8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/home/static/js/2.19709df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e6be03345479154e22",
+    "url": "/home/static/js/main.ff7b3323.chunk.js"
+  },
+  {
+    "revision": "36304743755371637be9",
+    "url": "/home/static/js/runtime-main.10d6ceab.js"
+  },
+  {
+    "revision": "bb49d4d518bfb20a52d82b7abbe7c68f",
+    "url": "/home/static/media/privacy.bb49d4d5.md"
+  },
+  {
+    "revision": "7cb010173e9d5d86d4dabe81ee3bceb3",
+    "url": "/home/static/media/terms.7cb01017.md"
+  }
+]);
